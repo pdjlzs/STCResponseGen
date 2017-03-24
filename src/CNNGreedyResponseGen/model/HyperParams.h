b9f5b35e15d762d0be562df97f5381e4273d2b29
DaPan <pdjlzs@live.com>
2017-03-24 21:21:14 +0800

    chore: add CNN RNN encoding file

---
+++ src/CNNGreedyResponseGen/model/HyperParams.h
@@ -0,0 +1,335 @@
+#ifndef SRC_HyperParams_H_
+#define SRC_HyperParams_H_
+
+#include "N3L.h"
+#include "Options.h"
+
+using namespace nr;
+using namespace std;
+
+struct HyperParams{
+	//required
+	int maxlength;
+	int action_num;
+	int maxCandidAction;
+	dtype delta;
+	unordered_map<string, vector<string> > trigram_candid;	// tri-gram dic for get actions candidate
+	unordered_map<string, vector<int> > random_tabel;		// each word have more than maxCandidAction wil have a random_tabel
+	unordered_map<string, int> triword_stat;	// tri-gram word freq for random tabel
+	unordered_map<string, int> word_stat;
+
+
+	unordered_map<string, float > uni_pro;	// ui-gram dic for calculate perplexity
+	unordered_map<string, float > uni_back;	// ui-gram dic for calculate perplexity
+	unordered_map<string, float > bi_pro;	// bi-gram dic for calculate perplexity
+	unordered_map<string, float > bi_back;	// bi-gram dic for calculate perplexity
+	unordered_map<string, float > tri_pro;	// tri-gram dic calculate perplexity
+
+
+	dtype nnRegular; // for optimization
+	dtype adaAlpha;  // for optimization
+	dtype adaEps; // for optimization
+	dtype dropProb;
+
+	//must assign
+	int word_context;
+	int word_hiddensize;
+	int word_rnnhiddensize;
+
+	int action_dim;
+	int action_hiddensize;
+	int action_rnnhiddensize;
+
+	int state_hiddensize;
+
+	int unk_strategy;
+
+	//auto generare
+	int word_dim;
+	int word_represent_dim;
+	int word_window;
+	int word_input2conv;
+	int state_represent_dim;
+public:
+	HyperParams(){
+		//maxlength = max_sentence_clength + 1;
+		bAssigned = false;
+	}
+
+public:
+	void setRequared(Options& opt, int noActionCode){
+		//please specify dictionary outside
+		//please sepcify char_dim, word_dim and action_dim outside.
+		maxlength = opt.maxLength;
+		action_num = noActionCode + opt.maxCandidAction;
+		maxCandidAction = opt.maxCandidAction;
+		delta = opt.delta;
+		bAssigned = true;
+
+		nnRegular = opt.regParameter;
+		adaAlpha = opt.adaAlpha;
+		adaEps = opt.adaEps;
+		dropProb = opt.dropProb;
+
+		word_context = opt.wordcontext;
+		word_hiddensize = opt.wordHiddenSize;
+		word_rnnhiddensize = opt.wordRNNHiddenSize;
+
+		action_dim = opt.actionEmbSize;
+		action_hiddensize = opt.actionHiddenSize;
+		action_rnnhiddensize = opt.actionRNNHiddenSize;
+
+		state_hiddensize = opt.state_hiddensize;
+
+		unk_strategy = opt.unkStrategy;
+
+	}
+
+	void loadModel(std::ifstream &is) {
+		is >> maxlength;
+		is >> action_num;
+		is >> maxCandidAction;
+		is >> delta;
+		is >> bAssigned;
+		is >> nnRegular;
+		is >> adaAlpha;
+		is >> adaEps;
+		is >> dropProb;
+		is >> word_context;
+		is >> word_hiddensize;
+		is >> word_rnnhiddensize;
+		is >> action_dim;
+		is >> action_hiddensize;
+		is >> action_rnnhiddensize;
+		is >> state_hiddensize;
+		is >> unk_strategy;
+		is >> word_dim;
+
+		int size = 0;
+		is >> size;
+		for (int i = 0; i < size; i++){
+			string bigram_word;
+			is >> bigram_word;
+			int tempsize;
+			is >> tempsize;
+			for (int j = 0; j < tempsize; j++){
+				string cur_word;
+				is >> cur_word;
+				trigram_candid[bigram_word].push_back(cur_word);
+			}
+		}
+
+		is >> size;
+		for (int i = 0; i < size; i++){
+			string cur_word;
+			is >> cur_word;
+			int freq;
+			is >> freq;
+			triword_stat[cur_word] = freq;
+		}
+
+		int table_size;
+		const float power = 0.75;
+		unordered_map<string, vector<string> >::iterator it;
+		for (it = trigram_candid.begin(); it != trigram_candid.end(); it++){
+			if (it->second.size() > maxCandidAction){
+				if (it->first == "-start-#-start-") table_size = 1e6;
+				//else if (it->second.size() >= 900) table_size = 1e5;
+				else if (it->second.size() >= 400) table_size = 1e5;
+				else table_size = 1e4;
+				random_tabel[it->first].resize(table_size);
+				double d1 = 0, train_words_pow = 0;
+				vector<int> vocab;
+				for (int i = 0; i < it->second.size(); i++){
+					string trigram_words = it->first + "#" + it->second[i];
+					int trigram_freq = triword_stat[trigram_words];
+					train_words_pow += pow(trigram_freq, power);
+					vocab.push_back(trigram_freq);
+				}
+				int i = 0;
+				d1 = pow(vocab[i], power) / train_words_pow;
+				for (int a = 0; a < table_size; a++) {
+					random_tabel[it->first][a] = i;
+					if (a / (double)table_size > d1) {
+						i++;
+						d1 += pow(vocab[i], power) / train_words_pow;
+					}
+					if (i >= vocab.size()) i = vocab.size() - 1;
+				}
+				int last_word_candi_index = random_tabel[it->first][table_size - 1];
+				if (last_word_candi_index != it->second.size() - 1)
+					cout << "Error! Not all candidate are in the random tabel" << last_word_candi_index << "!=" << it->second.size() - 1 << endl;
+			}
+		}
+
+
+		is >> size;
+		for (int i = 0; i < size; i++){
+			string cur_word;
+			is >> cur_word;
+			int freq;
+			is >> freq;
+			word_stat[cur_word] = freq;
+		}
+
+		is >> size;
+		for (int i = 0; i < size; i++){
+			string cur_word;
+			is >> cur_word;
+			float unipro;
+			is >> unipro;
+			uni_pro[cur_word] = unipro;
+		}
+
+		is >> size;
+		for (int i = 0; i < size; i++){
+			string cur_word;
+			is >> cur_word;
+			float uniback;
+			is >> uniback;
+			uni_back[cur_word] = uniback;
+		}
+
+		is >> size;
+		for (int i = 0; i < size; i++){
+			string cur_word;
+			is >> cur_word;
+			float bipro;
+			is >> bipro;
+			bi_pro[cur_word] = bipro;
+		}
+
+		is >> size;
+		for (int i = 0; i < size; i++){
+			string cur_word;
+			is >> cur_word;
+			float biback;
+			is >> biback;
+			bi_back[cur_word] = biback;
+		}
+
+		is >> size;
+		for (int i = 0; i < size; i++){
+			string cur_word;
+			is >> cur_word;
+			float tripro;
+			is >> tripro;
+			tri_pro[cur_word] = tripro;
+		}
+
+	}
+
+	void saveModel(std::ofstream &os) {
+		os << maxlength << endl;
+		os << action_num << endl;
+		os << maxCandidAction << endl;
+		os << delta << endl;
+		os << bAssigned << endl;
+		os << nnRegular << endl;
+		os << adaAlpha << endl;
+		os << adaEps << endl;
+		os << dropProb << endl;
+		os << word_context << endl;
+		os << word_hiddensize << endl;
+		os << word_rnnhiddensize << endl;
+		os << action_dim << endl;
+		os << action_hiddensize << endl;
+		os << action_rnnhiddensize << endl;
+		os << state_hiddensize << endl;
+		os << unk_strategy << endl;
+		os << word_dim << endl;
+
+		os << trigram_candid.size() << endl;
+		unordered_map<string, vector<string> >::iterator umit = trigram_candid.begin();
+		for (; umit != trigram_candid.end(); umit++){
+			os << umit->first << " ";
+			vector<string> &tempvec = umit->second;
+			os << tempvec.size() << " ";
+			os << tempvec[0];
+			for (int i = 1; i < tempvec.size(); i++){
+				os << " " << tempvec[i];
+			}
+			os << endl;
+		}
+
+		os << triword_stat.size() << endl;
+		unordered_map<string, int> ::iterator umit3 = triword_stat.begin();
+		for (; umit3 != triword_stat.end(); umit3++){
+			os << umit3->first << " " << umit3->second << endl;
+		}
+
+		os << word_stat.size() << endl;
+		unordered_map<string, int> ::iterator umit2 = word_stat.begin();
+		for (; umit2 != word_stat.end(); umit2++){
+			os << umit2->first << " " << umit2->second << endl;
+		}
+
+		os << uni_pro.size() << endl;
+		unordered_map<string, float> ::iterator umft1 = uni_pro.begin();
+		for (; umft1 != uni_pro.end(); umft1++){
+			os << umft1->first << " " << umft1->second << endl;
+		}
+
+
+		os << uni_back.size() << endl;
+		unordered_map<string, float> ::iterator umft11 = uni_back.begin();
+		for (; umft11 != uni_back.end(); umft11++){
+			os << umft11->first << " " << umft11->second << endl;
+		}
+
+		os << bi_pro.size() << endl;
+		unordered_map<string, float> ::iterator umft2 = bi_pro.begin();
+		for (; umft2 != bi_pro.end(); umft2++){
+			os << umft2->first << " " << umft2->second << endl;
+		}
+		os << bi_back.size() << endl;
+		unordered_map<string, float> ::iterator umft22 = bi_back.begin();
+		for (; umft22 != bi_back.end(); umft22++){
+			os << umft22->first << " " << umft22->second << endl;
+		}
+		os << tri_pro.size() << endl;
+		unordered_map<string, float> ::iterator umft3 = tri_pro.begin();
+		for (; umft3 != tri_pro.end(); umft3++){
+			os << umft3->first << " " << umft3->second << endl;
+		}
+
+
+	}
+	void clear(){
+		bAssigned = false;
+	}
+
+	bool bValid(){
+		return bAssigned;
+	}
+
+
+public:
+
+	void print(){
+		cout << "maxlength= " << maxlength << endl;
+		cout << "action_num= " << action_num << endl;
+		cout << "maxCandidAction= " << maxCandidAction << endl;
+		cout << "delta= " << delta << endl;
+		cout << "bAssigned= " << bAssigned << endl;
+		cout << "nnRegular= " << nnRegular << endl;
+		cout << "adaAlpha= " << adaAlpha << endl;
+		cout << "adaEps= " << adaEps << endl;
+		cout << "dropProb= " << dropProb << endl;
+		cout << "word_context= " << word_context << endl;
+		cout << "word_hiddensize= " << word_hiddensize << endl;
+		cout << "word_rnnhiddensize= " << word_rnnhiddensize << endl;
+		cout << "action_dim= " << action_dim << endl;
+		cout << "action_hiddensize= " << action_hiddensize << endl;
+		cout << "action_rnnhiddensize= " << action_rnnhiddensize << endl;
+		cout << "state_hiddensize= " << state_hiddensize << endl;
+		cout << "unk_strategy= " << unk_strategy << endl;
+		cout << "word_dim=" << word_dim << endl;
+	}
+
+private:
+	bool bAssigned;
+};
+
+
+#endif /* SRC_HyperParams_H_ */
---
+++ src/RNNGreedyResponseGen/model/ModelParams.h
@@ -0,0 +1,158 @@
+#ifndef SRC_ModelParams_H_
+#define SRC_ModelParams_H_
+
+#include "HyperParams.h"
+
+// Each model consists of two parts, building neural graph and defining output losses.
+class ModelParams {
+
+public:
+	//neural parameters
+	//parameters used in global node
+	Alphabet word_alpha; // words
+	LookupTable word_table; // should be initialized outside
+	Alphabet word_ext_alphas; // pre-train/external word embeddings
+	LookupTable word_ext_table; // should be initialized outside
+
+	UniParams word_conv; // word tanh after concat
+	LSTM1Params word_left_lstm; //left lstm
+	LSTM1Params word_right_lstm; //right lstm
+
+	//parameters used in action nodes
+	Alphabet action_alpha;
+	LookupTable action_table; // should be initialized outside
+	BiParams action_conv;
+	LSTM1Params action_lstm;
+
+	LookupTable scored_action_table;
+
+	UniParams state_hidden;
+public:
+	bool initial(HyperParams &opts, AlignedMemoryPool *mem) {
+
+		opts.word_dim = word_table.nDim;
+		opts.word_represent_dim = opts.word_dim * 2;
+		opts.word_window = 2 * opts.word_context + 1;
+		opts.word_input2conv = opts.word_represent_dim * opts.word_window;
+		opts.state_represent_dim = opts.word_rnnhiddensize * 2 + opts.action_rnnhiddensize;
+
+
+		word_conv.initial(opts.word_hiddensize, opts.word_input2conv, true, mem);
+		word_left_lstm.initial(opts.word_rnnhiddensize, opts.word_hiddensize, mem); 
+		word_right_lstm.initial(opts.word_rnnhiddensize, opts.word_hiddensize, mem);
+
+		action_conv.initial(opts.action_hiddensize, opts.action_dim, opts.action_dim, true, mem);
+		action_lstm.initial(opts.action_rnnhiddensize, opts.action_hiddensize, mem);
+		scored_action_table.initial(&action_alpha, opts.state_hiddensize, true);
+		state_hidden.initial(opts.state_hiddensize, opts.state_represent_dim, true, mem);
+
+		return true;
+	}
+
+	bool initial(HyperParams &opts){
+
+		opts.word_dim = word_table.nDim;
+		opts.word_represent_dim = opts.word_dim * 2;
+		opts.word_window = 2 * opts.word_context + 1;
+		opts.word_input2conv = opts.word_represent_dim * opts.word_window;
+		opts.state_represent_dim = opts.word_rnnhiddensize * 2 + opts.action_rnnhiddensize;
+		return true;
+	}
+
+	void exportModelParams(ModelUpdate &ada) {
+		//neural features
+		word_table.exportAdaParams(ada);
+		word_ext_table.exportAdaParams(ada);
+		word_conv.exportAdaParams(ada);
+		word_left_lstm.exportAdaParams(ada);
+		word_right_lstm.exportAdaParams(ada);
+		action_table.exportAdaParams(ada);
+
+		action_conv.exportAdaParams(ada);
+		action_lstm.exportAdaParams(ada);
+		state_hidden.exportAdaParams(ada);
+		scored_action_table.exportAdaParams(ada);
+	}
+
+	// will add it later
+	void saveModel(std::ofstream &os) {
+		word_alpha.write(os);
+		word_table.save(os);
+		word_ext_alphas.write(os);
+		word_ext_table.save(os);
+
+		word_conv.save(os);
+		word_left_lstm.save(os);
+		word_right_lstm.save(os);
+
+		action_alpha.write(os);
+		action_table.save(os);
+		action_conv.save(os);
+		action_lstm.save(os);
+
+		scored_action_table.save(os);
+
+		state_hidden.save(os);
+	}
+
+	void loadModel(std::ifstream &is, AlignedMemoryPool* mem = NULL) {
+		word_alpha.read(is);
+		word_table.load(is, &word_alpha, mem);
+		word_ext_alphas.read(is);
+		word_ext_table.load(is, &word_ext_alphas, mem);
+
+		word_conv.load(is, mem);
+		word_left_lstm.load(is, mem);
+		word_right_lstm.load(is, mem);
+
+		action_alpha.read(is);
+		action_table.load(is, &action_alpha, mem);
+		action_conv.load(is, mem);
+		action_lstm.load(is, mem);
+
+		scored_action_table.load(is, &action_alpha, mem);
+
+		state_hidden.load(is, mem);
+	}
+
+	void loadPretrainModel(std::ifstream &is, HyperParams &opts, AlignedMemoryPool* mem = NULL) {
+
+		opts.word_represent_dim = opts.word_dim * 2;
+		opts.word_window = 2 * opts.word_context + 1;
+		opts.word_input2conv = opts.word_represent_dim * opts.word_window;
+		opts.state_represent_dim = opts.word_rnnhiddensize * 2 + opts.action_rnnhiddensize;
+
+		word_conv.initial(opts.word_hiddensize, opts.word_input2conv, true, mem);
+		word_left_lstm.initial(opts.word_rnnhiddensize, opts.word_hiddensize, mem);
+		word_right_lstm.initial(opts.word_rnnhiddensize, opts.word_hiddensize, mem);
+
+		action_conv.initial(opts.action_hiddensize, opts.action_dim, opts.action_dim, true, mem);
+		action_lstm.initial(opts.action_rnnhiddensize, opts.action_hiddensize, mem);
+		state_hidden.initial(opts.state_hiddensize, opts.state_represent_dim, true, mem);
+
+		word_alpha.read(is);
+		word_table.initial(&word_alpha, opts.word_dim, true);
+		word_table.load(is, &word_alpha, mem);
+		word_ext_alphas.read(is);
+		word_ext_table.initial(&word_ext_alphas, opts.word_dim, false);
+		word_ext_table.load(is, &word_ext_alphas, mem);
+
+		word_conv.load(is, mem);
+		word_left_lstm.load(is, mem);
+		word_right_lstm.load(is, mem);
+
+		action_alpha.read(is);
+		action_table.initial(&action_alpha, opts.action_dim, true);
+		action_table.load(is, &action_alpha, mem);
+		action_conv.load(is, mem);
+		action_lstm.load(is, mem);
+
+		scored_action_table.initial(&action_alpha, opts.state_hiddensize, true);
+		scored_action_table.load(is, &action_alpha, mem);
+
+		state_hidden.load(is, mem);
+	}
+
+};
+
+#endif /* SRC_ModelParams_H_ */
